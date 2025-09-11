30cb3da243c523e5eec1caf8172ce37d2a0e8223
Neshadi Ranaweera <117301828+Neshadi@users.noreply.github.com>
2025-09-11 11:06:06 +0530

    Demo List fetch from database

--- src/Components/demo-list/demo-list.jsx
+++ src/Components/demo-list/demo-list.jsx
@@ -1,55 +1,45 @@
 import { Button } from '@material-ui/core';
-import React, { useRef, useState } from 'react';
-import demo10Image from '../../Assests/demo10.jpg';
-import demo2Image from '../../Assests/demo2.jpg';
-import demo3Image from '../../Assests/demo3.jpg';
-import demo5Image from '../../Assests/demo13.jpg';
-import demo4Image from '../../Assests/demo8.jpg';
-import demo1Image from '../../Assests/demo11.jpg';
+import React, { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
 import BookDemo from '../demo/demo';
-
-const demoListData = [
-  {
-    name: 'John Smith',
-    description: 'John is a certified personal trainer with a specialization in strength training and conditioning.With his extensive experience and knowledge, he helps clients achieve their fitness goals through personalized workout plans and motivation.',
-    imageUrl: demo1Image
-  },
-  {
-    name: 'Emil Johnson',
-    description: 'Emil is a dedicated fitness coach with expertise in yoga and flexibility training. He guides clients in improving their overall health and well-being and also his experience and motivation helps clients to achieve their fitness goals through .',
-    imageUrl: demo2Image
-  },
-  {
-    name: 'Michael Davis',
-    description: 'Michael is an esteemed group fitness instructor with a wealth of experience in guiding individuals towards their fitness goals. With a reputation for his high-energy and dynamic classes, Michael creates an environment where participants are motivated. ',
-    imageUrl: demo10Image
-  },
-  {
-    name: 'Jessic Brown',
-    description: 'Jessica is a certified nutrition coach who specializes in helping clients develop sustainable eating habits and achieve their weight loss goals. He provides personalized meal plans and ongoing support to optimize health and performance.',
-    imageUrl: demo4Image
-  },
-  {
-    name: 'Daniel Garcia',
-    description: 'Daniel is a knowledgeable martial arts instructor with expertise in various disciplines such as kickboxing and Muay Thai. He empowers clients with self-defense skills while promoting discipline, focus, courage, and confidence.',
-    imageUrl: demo5Image
-  },
-  {
-    name: 'Alexandra Martinez',
-    description: 'Alexandra has vibrant personality and infectious energy make her a standout cycling instructor who inspires and motivates her students with every class. With a passion for cycling that shines through in her teaching, she creates an atmosphere of excitement.',
-    imageUrl: demo3Image
-  },
-];
 
 const DemoList = () => {
   const listRef = useRef(null);
+  const [demos, setDemos] = useState([]);
   const [showBookDemo, setShowBookDemo] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+  // const [message, setMessage] = useState('');
+ 
+    
+
+  // Fetch demo list from backend
+  useEffect(() => {
+    const fetchDemos = async () => {
+       const token = localStorage.getItem('token');
+    if (!token) {
+      alert('No token found. Please log in.');
+      return;
+    }
+    const headers = {
+      'Authorization': `Bearer ${token}`
+    };
+      try {
+        const response = await axios.get("http://localhost:8080/demoList/findDemoList", {
+        headers: headers,
+      });
+        setDemos(response.data);
+      } catch (error) {
+        console.error("Error fetching demo list:", error);
+      }
+    };
+
+    fetchDemos();
+  }, []);
 
   const scrollLeft = () => {
     if (listRef.current) {
       listRef.current.scrollBy({
-        left: -400, // Adjust the scroll distance to match the width of 3 cards
+        left: -400,
         behavior: 'smooth',
       });
     }
@@ -58,7 +48,7 @@ const DemoList = () => {
   const scrollRight = () => {
     if (listRef.current) {
       listRef.current.scrollBy({
-        left: 400, // Adjust the scroll distance to match the width of 3 cards
+        left: 400,
         behavior: 'smooth',
       });
     }
@@ -73,8 +63,11 @@ const DemoList = () => {
       <h1 style={{ textAlign: 'center', margin: '10px' }}>OUR DEMOS</h1>
 
       <div style={{ position: 'relative' }}>
-        <div style={{ display: 'flex', overflow: 'hidden', scrollBehavior: 'smooth', paddingRight: '30px', paddingTop: '-10px', position: 'relative' }} ref={listRef}>
-          {demoListData.map((demo, index) => (
+        <div 
+          style={{ display: 'flex', overflow: 'hidden', scrollBehavior: 'smooth', paddingRight: '30px', paddingTop: '-10px', position: 'relative' }} 
+          ref={listRef}
+        >
+          {demos.map((demo, index) => (
             <div
               key={index}
               style={{
@@ -89,7 +82,7 @@ const DemoList = () => {
                 flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
-                transition: 'transform 0.3s, box-shadow 0.3s', // Add transition for smooth effect
+                transition: 'transform 0.3s, box-shadow 0.3s',
               }}
               className="demo-card"
               onMouseEnter={(e) => {
@@ -101,13 +94,15 @@ const DemoList = () => {
                 e.currentTarget.style.boxShadow = 'none';
               }}
             >
-              <img src={demo.imageUrl} alt={demo.name} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-              <h3 style={{ marginBottom: '8px', textAlign: 'center' }}>{demo.name}</h3>
-              <p style={{ marginBottom: '8px',  textAlign: 'justify' }}>{demo.description}</p>
+              <img src={`http://localhost:8080${demo.imageUrl}`} alt={demo.fullName} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+              <h3 style={{ marginBottom: '8px', textAlign: 'center' }}>{demo.fullName}</h3>
+              <p style={{ marginBottom: '8px', textAlign: 'justify' }}>{demo.description}</p>
+
             </div>
           ))}
         </div>
 
+        {/* Scroll buttons */}
         <div style={{ position: 'absolute', top: '50%', left: 0, transform: 'translateY(-50%)', width: '60px', height: '60px', background: 'white', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', cursor: 'pointer', zIndex: 2 }} onClick={scrollLeft}>
           <span style={{ color: 'black', fontSize: '24px', fontWeight: 'bold' }}>{'<'}</span>
         </div>
@@ -119,9 +114,13 @@ const DemoList = () => {
       <br />
       <div style={{ textAlign: 'center' }}>
         {showBookDemo ? <BookDemo /> : (
-          <Button variant="contained" style={{ color: 'white !important', fontWeight: 'bold', backgroundColor: isHovered ? 'red' : 'blue', width: '25%' }} onClick={handleClick}
+          <Button 
+            variant="contained" 
+            style={{ color: 'white !important', fontWeight: 'bold', backgroundColor: isHovered ? 'red' : 'blue', width: '25%' }} 
+            onClick={handleClick}
             onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}>
+            onMouseLeave={() => setIsHovered(false)}
+          >
             Book Demo
           </Button>
         )}
